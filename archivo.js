da8232ef819a6ea434deecad862959badb92a90b
Santiago Nicolas De Giacomi <santidegiacomi@gmail.com>
2022-10-28 13:18:04 -0300

    ultima version modificada

--- archivo.js
+++ archivo.js
@@ -41,7 +41,7 @@ console.log(ventaMensual2);
 
 //Autos (objetos) en stock en la concesionaria
 
-const auto1 = {id:2233, marca: "Volvo", modelo: "c30", color: "verde", precio: 60000, año: 2010}
+const auto1 = {id:2233, marca: "Volvo", modelo: "c30", color: "blanco", precio: 60000, año: 2010}
 const auto1Ajson = JSON.stringify(auto1);
 console.log(auto1Ajson);
 localStorage.setItem("mi auto1",auto1Ajson);
@@ -49,7 +49,7 @@ const auto1AlStorage = localStorage.getItem("mi auto1");
 const parsearAuto1 = JSON.parse(auto1AlStorage);
 console.log(parsearAuto1);
 
-const auto2 = {id:1155, marca: "Porsche", modelo: "Cayenne", color: "rojo", precio: 80000, año: 2016}
+const auto2 = {id:1155, marca: "Porsche", modelo: "Cayenne", color: "gris metalico", precio: 80000, año: 2016}
 const auto2Ajson = JSON.stringify(auto2);
 console.log(auto2Ajson);
 localStorage.setItem("mi auto2", auto2Ajson);
@@ -65,10 +65,56 @@ const auto3AlStorage = localStorage.getItem("mi auto3");
 const parsearAuto3 = JSON.parse(auto3AlStorage);
 console.log(parsearAuto3);
 
-const auto4 = {id:2678, marca: "Mazda", modelo: "rx7", color: "naranja", precio: 30000, año: 1998}
+const auto4 = {id:2678, marca: "Mazda", modelo: "rx7", color: "negro", precio: 30000, año: 1998}
 const auto4Ajson = JSON.stringify(auto4);
 console.log(auto4Ajson);
 localStorage.setItem("mi auto4",auto4Ajson);
 const auto4AlStorage = localStorage.getItem("mi auto4");
 const parsearAuto4 = JSON.parse(auto4AlStorage);
 console.log(parsearAuto4);
+
+//Incorporo 2 cards con informacion de los empleados
+
+const empleadoUno = { id:01,
+                    nombre: "Juan Ramon Perez",
+                    puesto: "Asesor Comercial.",
+                    telefono: 1588234567,
+                    descripcion: "Años de experiencia en el rubro automotor al servicio de la gente.",
+                    imagen: "./images/JuanRamonPerez.jpg"
+}
+let articuloTarjeta = document.getElementById("tarjetaUno");
+
+articuloTarjeta.innerHTML = `
+        <div>
+            <div id="tarjetaUno" class="card" style="width: 18rem;">
+                <img src="${empleadoUno.imagen}" class="card-img-top" alt="${empleadoUno.puesto}">
+                <div class="card-body">
+                    <h5 class="card-title">${empleadoUno.nombre}</h5>
+                    <p class="card-text">${empleadoUno.descripcion}</p>
+                    <p class="card-text"> Ante cualquier inconveniente mi numero de celular es: ${empleadoUno.telefono}</p>
+                    <a button="miBoton" class="btn btn-primary">Contactar</a>
+                </div>
+        </div>
+`
+const empleadoDos = {   id:02,
+                        nombre: "Jorgelina Minuzzi",
+                        puesto: "Asesora Comercial",
+                        telefono: 1533445213,
+                        descripcion: "Aseguro calidad en el servicio y buena atencion. Mi reputacion en el rubro automotor es intachable",
+                        imagen: "./images/JorgelinaMinuzzi.jpg"
+}
+let otroArticuloTarjeta = document.getElementById("tarjetaDos");
+
+otroArticuloTarjeta.innerHTML = `
+            <div id="tarjetaDos" class="card" style="width: 18rem;">
+                <img src="${empleadoDos.imagen}" class="card-img-top" alt="${empleadoDos.puesto}">
+                <div class="card-body">
+                    <h5 class="card-title">${empleadoDos.nombre}</h5>
+                    <p class="card-text">${empleadoDos.descripcion}</p>
+                    <p class="card-text">Ante cualquier inconveniente mi numero de celular es: ${empleadoDos.telefono}</p>
+                    <a button="miBoton2" class="btn btn-primary">Contactar</a>
+                </div>
+            </div>
+`;
+
+
