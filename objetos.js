0eb7dbe5babf5e77245a235f439929324b03cee4
Santiago Nicolas De Giacomi <santidegiacomi@gmail.com>
2022-11-05 18:01:41 -0300

    proyecto encaminado

---
+++ objetos.js
@@ -0,0 +1,53 @@
+const empleadosConcesionaria = [{   
+                    id:01,
+                    nombre: "Juan Ramon Perez",
+                    puesto: "Asesor Comercial.",
+                    telefono: 1588234567,
+                    descripcion: "Años de experiencia en el rubro automotor al servicio de la gente.",
+                    imagen:"../imagenes/JuanRamonPerez.jpg"
+},
+{                   id:02,
+                    nombre: "Jorgelina Minuzzi",
+                    puesto: "Asesora Comercial",
+                    telefono: 1533445213,
+                    descripcion: "Aseguro calidad en el servicio y buena atencion. Mi reputacion en el rubro automotor es intachable",
+                    imagen:"../imagenes/jorMinuzzi.jpg"
+}
+]
+const vehiculosEnVenta = [{
+                id:2233, 
+                marca: "Volvo", 
+                imagen:"../imagenes/volvoBlanco.jpg",
+                modelo: "C30", 
+                color: "Blanco", 
+                precio: 60000, 
+                anio: 2010
+},
+{
+                id:1155, 
+                marca: "Porsche", 
+                imagen: "../imagenes/porschecayenne.jpg",
+                modelo: "Cayenne", 
+                color: "Gris Metalico", 
+                precio: 80000, 
+                anio: 2016
+},
+{
+                id:5555, 
+                marca: "Ford",
+                imagen: "../imagenes/fordmustang.jpg", 
+                modelo: "Mustang", 
+                color: "Azul Metalico", 
+                precio: 100000, 
+                anio: 2005
+},
+{
+                id:2678, 
+                marca: "Mazda", 
+                imagen: "../imagenes/mazdarx7.jpg",
+                modelo: "rx7", 
+                color: "Negro", 
+                precio: 30000, 
+                anio: 1998
+}
+]
