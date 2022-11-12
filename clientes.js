61923a5e43f9e22185de306633514a4127ad876f
Santiago Nicolas De Giacomi <santidegiacomi@gmail.com>
2022-11-12 19:43:00 -0300

    carga de datos desde un json local

---
+++ clientes.js
@@ -0,0 +1,22 @@
+// Cargo datos desde un JSON local (clientes.json) usando un metodo GET
+function obtenerDatosJson(){
+    const URLJSON = "/clientes.json"
+    fetch(URLJSON)
+    .then(reciboDatos => reciboDatos.json())
+    .then(datosRecibidos =>{
+        const misClientes = datosRecibidos.clientes
+        misClientes.forEach(cliente => {
+        document.getElementById("tablaClientes").innerHTML += `
+        <tr>
+        <th scope="row">${cliente.Id}</th>
+        <td>${cliente.Nombre}</td>
+        <td>${cliente.Apellido}</td>
+        <td>${cliente.Edad} Años</td>
+        <td>${cliente.Antiguedad} Años</td>
+        <td>${cliente.Auto}</td>
+    </tr>
+`;
+        })
+})
+}
+obtenerDatosJson();
