76351ad81ee78f288863345861482c700383f717
Santiago Nicolas De Giacomi <santidegiacomi@gmail.com>
2022-11-17 15:24:30 -0300

    ultima version

--- carrito.js
+++ carrito.js
@@ -1,51 +1,51 @@
-//console.log(de vehiculos)
-let vehiculosJSON=[];
+//console.log de los vehiculos
+let vehiculosJSON = [];
 let dolarCompra;
-//agrego storage carrito
+//Agrego storage para carrito
 let totalCarrito;
 let contenedor = document.getElementById("vehiculosVenta");
 let botonFinalizar = document.getElementById("botonFinalizar");
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-(carrito.length !=0)&&miTabla();
+(carrito.length != 0) && miTabla();
 obtenerDolar();
 
 function miTabla(){
     for(const vehiculo of carrito){
-        document.getElementById("tablaBody").innerHTML += `
+        document.getElementById("tablaBody"). innerHTML += `
         <tr>
         <td>${vehiculo.id}</td>
         <td>${vehiculo.marca}</td>
         <td>${vehiculo.modelo}</td>
-        <td>$ ${vehiculo.precio}</td>
+        <td>${vehiculo.precio}</td>
         <td>${vehiculo.color}</td>
         <td>${vehiculo.anio}</td>
-        <td><button class="btn btn-light" onclick="eliminar(event)">🗑️</button></td>
-    </tr>
-    `;
+        <td><button class="btn btn-light" onclick="eliminar(event)"</button></td>
+        </tr>
+        `;
     }
-    totalCarrito = carrito.reduce((acumulador,vehiculo) => acumulador +vehiculo.precio ,0 );
+    totalCarrito = carrito.reduce((acumulador,vehiculo) => acumulador + vehiculo.precio,0);
     let total = document.getElementById("total");
-    total.innerText="Total a pagar $= " + totalCarrito;
+    total.innerText = "Total a pagar $= " + totalCarrito;
 }
 
 function renderizarVehiculos(){
-        for(const vehiculo of vehiculosJSON){
-    contenedor.innerHTML += `
-    <div class="card col-sm-2">
-        <img src="${vehiculo.imagen}" class="card-img-top" alt="${vehiculo.anio}">
-        <div class="card-body">
-            <h5 class="card-title">${vehiculo.marca} ${vehiculo.modelo}</h5>
-            <p class="card-text">${vehiculo.color}</p>
-            <p class="card-text">${vehiculo.id}</p>
-            <p class="card-text">U$D ${vehiculo.precio/dolarCompra.toFixed(2)}</p>
-            <button id="btn ${vehiculo.id}" class="btn btn-primary">Comprar</button>
+    for(const vehiculo of vehiculosJSON){
+        contenedor.innerHTML += `
+        <div class="card col-sm-2">
+        <img src= "${vehiculo.imagen}" class="card-img-top" alt= "${vehiculo.anio}">
+        <div class= "card-body">
+        <h5 class="card-title">${vehiculo.marca} ${vehiculo.modelo}</h5>
+        <p class= "card-text">${vehiculo.color}</p>
+        <p class= "card-text">${vehiculo.id}</p>
+        <p class= "card-text">U$D
+        ${vehiculo.precio/dolarCompra.toFixed(2)}</p>
+        <button id="btn ${vehiculo.id}" class="btn btn-primary">Comprar</button>
         </div>
-    </div>
-    `;
+        `;
     }
-        }
-//Eventos
+}
+//eventos
 vehiculosJSON.forEach(vehiculo => {
     document.getElementById(`btn${vehiculo.id}`).addEventListener("click", function(){
         agregarAlCarrito(vehiculo);
@@ -71,17 +71,17 @@ function agregarAlCarrito(vehiculoComprado){
             )
             }
         })
-        document.getElementById("tablaBody").innerHTML += `
-        <tr>
-        <td>${vehiculo.id}</td>
-        <td>${vehiculo.marca}</td>
-        <td>${vehiculo.modelo}</td>
-        <td>$ ${vehiculo.precio}</td>
-        <td>${vehiculo.color}</td>
-        <td>${vehiculo.anio}</td>                
-    </tr>
-    `;
-    totalCarrito = carrito.reduce((acumulador,vehiculo) => acumulador + vehiculo.precio,0 );
+document.getElementById("tablaBody").innerHTML += `
+<tr>
+<td>${vehiculoAComprar.id}</td>
+<td>${vehiculoAComprar.marca}</td>
+<td>${vehiculoAComprar.modelo}</td>
+<td>$ ${vehiculoAComprar.precio}</td>
+<td>${vehiculoAComprar.color}</td>
+<td>${vehiculoAComprar.anio}</td>                
+</tr>
+`;
+totalCarrito = carrito.reduce((acumulador,vehiculo) => acumulador + vehiculo.precio,0 );
     let infoTotal = document.getElementById("total");
     infoTotal.innerText = "Total a pagar $: " + totalCarrito;
     localStorage.setItem("carrito", JSON.stringify(carrito));
@@ -117,14 +117,13 @@ function obtenerDolar(){
         const dolarBlue = cotizaciones.blue;
         console.log(dolarBlue);
         document.getElementById("api_dolar").innerHTML += `
-        <p>Dolar compra: $ ${dolarBlue.value_buy} Dolar venta: $ ${dolarBlue.value_sell}</p>
+        <p>Dolar compra: $ ${dolarBlue.value_buy} Dolar venta: $ ${dolarBlue.value_buy}</p>
         `;
         dolarCompra = dolarBlue.value_buy;
         obtenerJSON();
     })
 }
-
-//getJSON de vehiculos.JSON
+//getJson de vehiculos.json
 async function obtenerJSON(){
     const URLJSON = "vehiculos.json"
     const resp = await fetch (URLJSON);
@@ -132,14 +131,14 @@ async function obtenerJSON(){
     vehiculosJSON = data;
     renderizarVehiculos();
 }
-//Finalizar la compra
+//finalizar la compra
 botonFinalizar.onclick = () => {
-    if(carrito.length==0){
+    if(carrito.length ==0){
         Swal.fire({
             icon: 'error',
             title: 'El carro esta vacio',
-            text: 'Necesita comprar algun vehiculo!',
+            text: 'Necesita compra algun vehiculo!!',
             footer: '<a href="">Cual es el problema?</a>'
-        })
+            })    
     }
 }
