4671d7f71b1a348cbbc76ab51b83a1e10021e966
Santiago Nicolas De Giacomi <santidegiacomi@gmail.com>
2022-11-18 12:04:46 -0300

    carrito version final

--- carrito.js
+++ carrito.js
@@ -1,7 +1,6 @@
 
     //console.log(productos);
     let vehiculosJSON=[];
-    let dolarCompra;
     //Agregado storage carrito
     let totalCarrito;
     let contenedor = document.getElementById("vehiculosVenta");
@@ -9,7 +8,6 @@
     let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
     
     (carrito.length != 0)&&miTabla();
-    obtenerDolar();
 
 
     const DateTime = luxon.DateTime;
@@ -35,29 +33,32 @@
     }
 
     function renderizarVehiculos(){
-        for(const vehiculo of vehiculosJSON){
-            contenedor.innerHTML += `
-            <div class="card col-sm-2">
-            <img src="${vehiculo.imagen}" class="card-img-top" alt="${vehiculo.anio}">
-            <div class="card-body">
-                <h5 class="card-title">${vehiculo.marca} ${vehiculo.modelo}</h5>
-                <p class="card-text">${vehiculo.color}</p>
-                <p class="card-text">${vehiculo.id}</p>
-                <p class="card-text">U$D ${vehiculo.precio/dolarCompra.toFixed(2)}</p>
-                <button id="btn ${vehiculo.id}" class="btn btn-primary">Comprar</button>
+        fetch(`../vehiculos.json`)
+        .then(resp => resp.json())
+        .then(listaProd => {
+            listaProd.forEach(vehiculo =>{
+                contenedor.innerHTML += `
+                <div class="card col-sm-2">
+                <img src="${vehiculo.imagen}" class="card-img-top" alt="${vehiculo.anio}">
+                <div class="card-body">
+                    <h5 class="card-title">${vehiculo.marca} ${vehiculo.modelo}</h5>
+                    <p class="card-text">${vehiculo.color}</p>
+                    <p class="card-text">${vehiculo.id}</p>
+                    <p class="card-text">U$D ${vehiculo.precio}</p>
+                    <button id="btn${vehiculo.id}" class="btn btn-primary">Comprar</button>
+                </div>
             </div>
-        </div>
-            `;
-        }
-
-        //EVENTOS
-        vehiculosJSON.forEach(vehiculo => {
-            //evento para cada boton
-            document.getElementById(`btn${vehiculo.id}`).addEventListener("click",function(){
-                agregarAlCarrito(vehiculo);
-            });
+                `;
+            })
+            listaProd.forEach(vehiculo => {
+                //evento para cada boton
+                document.getElementById(`btn${vehiculo.id}`).addEventListener("click",function(){
+                    agregarAlCarrito(vehiculo);
         })
-    }
+        })
+            })
+        }
+    renderizarVehiculos()
 
     function agregarAlCarrito(vehiculoComprado){
         carrito.push(vehiculoComprado);
@@ -92,16 +93,11 @@
 
     //Para eliminar prods del carro
     function eliminar(ev){
-        console.log(ev);
         let fila = ev.target.parentElement.parentElement;
-        console.log(fila);
         let id = fila.children[0].innerText;
-        console.log(id);
         let indice = carrito.findIndex(vehiculo => vehiculo.id == id);
-        console.log(indice)
         //remueve el producto del carro
         carrito.splice(indice,1);
-        console.table(carrito);
         //remueve la fila de la tabla
         fila.remove();
         //recalcular el total
@@ -111,31 +107,6 @@
         localStorage.setItem("carrito",JSON.stringify(carrito));
     }
 
-    //Obtener valor dolar
-    function obtenerDolar(){
-        const URLDOLAR="https://api.bluelytics.com.ar/v2/latest";
-        fetch(URLDOLAR)
-            .then( respuesta => respuesta.json())
-            .then( cotizaciones => {
-                const dolarBlue = cotizaciones.blue;
-                console.log(dolarBlue);
-                document.getElementById("api_dolar").innerHTML+=`
-                    <p>Dolar compra: $ ${dolarBlue.value_buy} Dolar venta: $ ${dolarBlue.value_sell}</p>
-                `;
-                dolarCompra=dolarBlue.value_buy;
-                obtenerJSON();
-            })
-    }
-
-    //GETJSON de vehiculos.json
-    async function obtenerJSON() {
-        const URLJSON="../vehiculos.json";
-        const resp = await fetch(URLJSON);
-        const data = await resp.json();
-        productosJSON = data;
-        //ya tengo el dolar y los productos, renderizo las cartas
-        renderizarVehiculos();
-    }
 
     //Cerrando al compra
     botonFinalizar.onclick = () => {
@@ -149,14 +120,14 @@
             })
         }else{
             carrito = [];
-            document.getElementById("tablabody").innerHTML="";
+            document.getElementById("tablaBody").innerHTML="";
             let infoTotal = document.getElementById("total");
             infoTotal.innerText="Total a pagar $: ";
             Toastify({
                 text: "Aguarde y recibirá un email para confirmar la compra",
-                duration: 2000,
+                duration: 3000,
                 gravity: 'bottom',
-                position: 'center',
+                position: 'left',
                 style: {
                     background: 'linear-gradient(to right, lightgreen, black)'
                 }
@@ -170,4 +141,4 @@
             localStorage.removeItem("carrito");
         }
     }
-
+    
